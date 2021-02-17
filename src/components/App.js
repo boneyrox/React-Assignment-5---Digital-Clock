a609a5d03cc6a21ce0543ac6513898454d79f9db
BONEY UPADHYAY <boney003.upadhyay@gmail.com>
2021-02-17 19:09:04 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "./../styles/App.css";
+let time = new Date();
 
-class App extends Component {
-    render() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hours: time.getHours(),
+      minutes: time.getMinutes(),
+      seconds: time.getSeconds()
+    };
+  }
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <div className="Clock">
+        {this.state.hours}:{this.state.minutes}:{this.state.seconds}{" "}
+        {this.state.hours >= 12 ? "PM" : "AM"}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      let t = new Date();
+      this.setState({
+        hours: t.getHours(),
+        minutes: t.getMinutes(),
+        seconds: t.getSeconds()
+      });
+    }, 1000);
+  }
 }
-
-
-export default App;
